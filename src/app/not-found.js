f0ff52bd8e97d16c73b78c849f05039535f8efbd
yassine <yassineova@gmail.com>
2024-12-16 00:12:59 +0100

    change something

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,14 @@
 import { Link } from "lucide-react";
 
 export default function NotFound() {
-    return <div class="bg-gradient-to-r from-slate-200 to-gray-200 dark:from-gray-800 dark:to-gray-900 text-black dark:text-white">
-    <div class="flex items-center justify-center min-h-screen px-2">
-      <div class="text-center">
-        <h1 class="text-9xl font-bold">404</h1>
-        <p class="text-2xl font-medium mt-4">Oops! Page not found</p>
-        <p class="mt-4 mb-8">The page you`re looking for doesn`t exist or has been moved.</p>
+    return <div className="bg-gradient-to-r from-slate-200 to-gray-200 dark:from-gray-800 dark:to-gray-900 text-black dark:text-white">
+    <div className="flex items-center justify-center min-h-screen px-2">
+      <div className="text-center">
+        <h1 className="text-9xl font-bold">404</h1>
+        <p className="text-2xl font-medium mt-4">Oops! Page not found</p>
+        <p className="mt-4 mb-8">The page you`re looking for doesn`t exist or has been moved.</p>
         <Link href="/"
-          class="px-6 py-3 bg-white font-bold font-semibold rounded-full hover:bg-purple-100 transition duration-300 ease-in-out dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white">
+          className="px-6 py-3 bg-white font-bold font-semibold rounded-full hover:bg-purple-100 transition duration-300 ease-in-out dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white">
           Go Back Home
         </Link>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,30 +3,30 @@ import { Link } from "next/link";
 export default function NavBar() {
   return (
     <header
-    class="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
-    <div class="px-4">
-        <div class="flex items-center justify-between">
-            <div class="flex shrink-0">
-                <Link aria-current="page" class="flex items-center" href="/">
-                    {/* <img class="h-7 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt=""> */}
-                    <p class="sr-only">Website Title</p>
+    className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+    <div className="px-4">
+        <div className="flex items-center justify-between">
+            <div className="flex shrink-0">
+                <Link aria-current="page" className="flex items-center" href="/">
+                    {/* <img className="h-7 w-auto" src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt=""> */}
+                    <p className="sr-only">Website Title</p>
                 </Link>
             </div>
-            <div class="hidden md:flex md:items-center md:justify-center md:gap-5">
+            <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
                 <Link aria-current="page"
-                    class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                    className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
                     href="/">Home</Link>
-                <Link class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
                     href="#">About</Link>
-                <Link class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
                     href="/Blog">Blog</Link>
-                <Link class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
                     href="#">Contact us</Link>
             </div>
-            <div class="flex items-center justify-end gap-3">
-                <Link class="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
+            <div className="flex items-center justify-end gap-3">
+                <Link className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
                     href="/login">Sign in</Link>
-                <Link class="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                <Link className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                     href="/login">Login</Link>
             </div>
         </div>
