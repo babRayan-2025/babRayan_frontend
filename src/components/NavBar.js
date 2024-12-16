616e2573a322518950896b28cb750336e3a1694a
mouad fanine <mouadfanine@gmail.com>
2024-12-16 14:54:12 +0100

    add pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ export default function NavBar() {
             </Link>
             <Link
               className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-              href="#"
+              href="/about"
             >
               About
             </Link>
@@ -33,7 +33,7 @@ export default function NavBar() {
             </Link>
             <Link
               className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-              href="#"
+              href="/contact_us"
             >
               Contact us
             </Link>
@@ -41,7 +41,7 @@ export default function NavBar() {
           <div className="flex items-center justify-end gap-3">
             <Link
               className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-              href="/login"
+              href="/register"
             >
               Sign in
             </Link>
