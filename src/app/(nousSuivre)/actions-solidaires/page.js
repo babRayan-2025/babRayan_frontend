8cbc61ed63b0754328b7991806ff4b7f41e2231b
yassine <yassineova@gmail.com>
2025-01-29 01:40:26 +0100

    actions solidaires

--- src/app/(nousSuivre)/actions-solidaires/page.js
+++ src/app/(nousSuivre)/actions-solidaires/page.js
@@ -11,6 +11,34 @@ const fadeIn = {
 };
 
 export default function Actions() {
+
+    const ftourPhotos = [
+        { src: "./actions_solidaires/15.png", alt: "Ramadan" },
+        { src: "./actions_solidaires/14.png", alt: "Ramadan" },
+        { src: "./actions_solidaires/12.png", alt: "Ramadan" },
+        { src: "./actions_solidaires/13.png", alt: "Ramadan" },
+    ]
+
+    const seismePhotos = [
+        { src: "./actions_solidaires/9.png", alt: "Seisme" },
+        { src: "./actions_solidaires/10.png", alt: "Seisme" },
+        { src: "./actions_solidaires/11.png", alt: "Seisme" },
+    ]
+
+    const scolairePhotos = [
+        { src: "./actions_solidaires/7.png", alt: "Scolaire" },
+        { src: "./actions_solidaires/8.png", alt: "Scolaire" },
+        { src: "./actions_solidaires/8.png", alt: "Scolaire" },
+        { src: "./actions_solidaires/8.png", alt: "Scolaire" },
+    ]
+    const actionsPhotos = [
+        { src: "./actions_solidaires/1.png", alt: "action" },
+        { src: "./actions_solidaires/2.png", alt: "action" },
+        { src: "./actions_solidaires/3.png", alt: "action" },
+        { src: "./actions_solidaires/4.png", alt: "action" },
+        { src: "./actions_solidaires/5.png", alt: "action" },
+        { src: "./actions_solidaires/6.png", alt: "action" }
+    ]
     return (
         <main>
             <motion.h1
@@ -26,30 +54,165 @@ export default function Actions() {
                 Depuis sa création, l'Association Bab Rayan ne se contente pas seulement de veiller au bien-être des enfants à travers ses trois projets principaux. Elle s'engage également dans une série d'actions solidaires, démontrant ainsi son engagement envers la communauté et sa volonté de créer un impact positif au-delà de ses programmes établis.
             </div>
 
+
+
             <motion.div
                 initial="hidden"
                 animate="visible"
                 variants={fadeIn}
-                className="bg-red-700 p-8" >
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-                    <div className="p-4">
-                        <h1 className="text-yellow-200 text-4xl font-bold ">
-                            Ftour <br /> Bab Rayan
-                        </h1><br />
-                        <p className="text-white mb-2">
-                            Les Actions du « Ftour Bab Rayan » représentent l'ADN même de l'association, constituant sa première et plus ancienne initiative.
-                        </p><br />
-                        <p className="text-white mb-2">
-                            Les familles en précarité bénéficiaires ne manquent jamais cette occasion, et nous non plus, car nous les considérons comme une part intégrante de notre engagement durant ce mois sacré.
-                        </p>
+                className="bg-red-700 p-8"
+            >
+                <div className="flex flex-col xl:flex-row items-center gap-8">
+                    {/* Image Section */}
+                    <img
+                        src="./actions_solidaires/ramadan.png"
+                        alt="Ramadan"
+                        className="absolute top-0 right-0 w-64 h-64 object-cover"
+                    />
+
+                    {/* Text Section */}
+                    <div className="w-full lg:w-1/2 text-white xl:ps-80">
+                        <div className="relative col-span-1 lg:col-span-2  h-full flex flex-col justify-center items-start">
+                            {/* Image positioned in the top-right corner */}
+                            <img
+                                src="./actions_solidaires/ramadan.png"
+                                alt="Ramadan"
+                                className="absolute top-0 right-0 w-64 h-64 object-cover"
+                            />
+
+                            {/* Content */}
+                            <div>
+                                <h1 className="text-yellow-200 text-4xl font-bold">
+                                    Ftour <br /> Bab Rayan
+                                </h1>
+                                <br />
+                                <p className="text-white mb-2">
+                                    Les Actions du « Ftour Bab Rayan » représentent l'ADN même de l'association, constituant sa première et plus ancienne initiative.
+                                </p>
+                                <br />
+                                <p className="text-white mb-2">
+                                    Les familles en précarité bénéficiaires ne manquent jamais cette occasion, et nous non plus, car nous les considérons comme une part intégrante de notre engagement durant ce mois sacré.
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="bg-white rounded-lg shadow-lg p-4">
-                        <h2 className="text-xl font-bold mb-2">Titre 2</h2>
+                    <div className="grid grid-cols-2 gap-4 w-full xl:w-1/2 xl:pe-80">
+                        {ftourPhotos.map((photo, index) => (
+                            <div key={index} className="overflow-hidden  ">
+                                <img
+                                    src={photo.src}
+                                    alt={photo.alt}
+                                    style={{ width: "400px" }}
+                                    className="w-full h-auto object-cover"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </motion.div>
+
+
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={fadeIn}
+                className="bg-yellow-200 p-8"
+            >
+                <div className="items-center gap-8 xl:px-80">
+                    <div className="text-red-700 text-2xl md:text-4xl font-bold my-8">
+                        Séisme <br /> D'Alhaouz
+                    </div>
+                    <p className="text-black text-xl">
+                        En plus des activités mentionnées, l'association intervient également en cas d'urgence nationale, telles que la pandémie de Covid-19 ou le séisme dans la région d'Al Haouz en septembre dernier.
+                        <br />La solidarité massive démontrée à travers le Maroc s'inspire de notre Souverain, Sa Majesté le Roi Mohammed VI
+                    </p>
+
+                    <div className="flex justify-center items-center gap-4 my-8">
+                        {seismePhotos.map((photo, index) => (
+                            <div key={index} className="w-1/3 overflow-hidden rounded-lg shadow-md">
+                                <img
+                                    src={photo.src}
+                                    alt={photo.alt}
+                                    className="w-full h-full object-cover rounded-lg"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </motion.div>
+
+
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={fadeIn}
+                className="bg-red-700 p-8"
+            >
+                <div className="flex flex-col lg:flex-row items-center gap-8">
+                    {/* Image Section */}
+                    <div className="grid grid-cols-2 gap-4 w-full lg:w-1/2 lg:ps-80">
+                        {scolairePhotos.map((photo, index) => (
+                            <div key={index} className="overflow-hidden  ">
+                                <img
+                                    src={photo.src}
+                                    alt={photo.alt}
+                                    style={{ width: "300px" }}
+                                    className="w-full h-auto object-cover"
+                                />
+                            </div>
+                        ))}
                     </div>
 
+                    {/* Text Section */}
+                    <div className="w-full lg:w-1/2 text-white">
+                        <h1 className="text-yellow-200 text-4xl font-bold">
+                            Service <br /> Scolaires
+                        </h1>
+                        <br />
+                        <p className="text-white mb-2">
+                            Depuis la création de l'école palmier en 2018, l'association Bab Rayan fournit :
+
+                        </p>
+                        <br />
+                        <p className="text-white mb-2">
+                            &nbsp; &nbsp; Des cartables garnis de fournitures scolaires et manuels à tous les bénéficiaires de l'école, soit aux (internes enfants résident au foyer Bab Rayan) ou aux externes (enfants issus de familles en grande précarité).
+                            <br />Un repas équilibré et une garde gratuite. <br />
+                            Soutien scolaire gratuit. <br />
+                            Activités et excursions ludiques gratuites.
+                        </p>
+                    </div>
+                </div>
+            </motion.div>
+
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={fadeIn}
+                className="bg-yellow-200 p-8"
+            >
+                <div className="items-center gap-8 xl:px-80">
+                    <div className="text-red-700 text-2xl md:text-4xl font-bold my-8">
+                        Actions <br /> Diverses
+                    </div>
+                    <p> cLe pôle Actions Solidaires propose divers services sociaux, incluant la circoncision, le soutien aux familles pendant l'Aid al-Adha, et inculquer à nos entants du foyer Bab Rayan nos valeurs solidaires par le biais de partage de dons en nature, reçus de nos partenaires et donateurs, aux familles en besoins.
+                    </p>
+
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8">
+                        {actionsPhotos.map((photo, index) => (
+                            <div key={index} className="overflow-hidden ">
+                                <img
+                                    src={photo.src}
+                                    alt={photo.alt}
+                                    className="w-full h-auto object-cover"
+                                />
+                            </div>
+                        ))}
+                    </div>
 
                 </div>
             </motion.div>
+
+
         </main>
     );
 }
