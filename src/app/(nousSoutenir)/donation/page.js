36fcbc606b371ed07a3fd05967d626c7fddc5872
yassine <yassineova@gmail.com>
2025-02-02 13:12:19 +0100

    Update page.js

--- src/app/(nousSoutenir)/donation/page.js
+++ src/app/(nousSoutenir)/donation/page.js
@@ -9,6 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 const fadeIn = {
   hidden: { opacity: 0, y: 20 },
   visible: { 
+    opacity: 1, 
     y: 0,
     transition: { duration: 0.6 }
   }
