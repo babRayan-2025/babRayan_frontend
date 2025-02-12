a082eddb99c5cb0eb02b71f043347c594474ff35
Mouad Fanine <91895127+MFanine@users.noreply.github.com>
2025-02-12 05:18:10 +0100

    benevoles form changes

--- src/app/(nousSoutenir)/benevole/page.js
+++ src/app/(nousSoutenir)/benevole/page.js
@@ -1,8 +1,15 @@
 "use client";
+
+import React, { useState } from 'react';
 import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Benevole() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (item) => {
+    setSelectedButton(item);
+  };
   return (
     <main className="">
       <motion.div
@@ -106,150 +113,161 @@ export default function Benevole() {
 
       {/* Form Section */}
       <motion.div
-        className="w-full bg-[url('/benevole/bg-benevole.png')] bg-cover bg-center py-16 px-16"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 1.2 }}
-      >
-        <h1 className="text-white text-6xl md:text-4xl font-bold mx-auto pl-8">
-          Formulaire
-          <div className="w-44 md:w-44 h-1 bg-yellow-300 mt-2"></div>
-        </h1>
-        <div className="mx-auto p-8">
-          {/* Form Fields */}
-          <motion.div
-            className="grid grid-cols-1 md:grid-cols-2 text-gray-800 gap-6 mb-8"
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1, delay: 1.5 }}
-          >
-            <input
-              type="text"
-              placeholder="Nom *"
-              className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
-            />
-            <input
-              type="email"
-              placeholder="Email *"
-              className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
-            />
-            <input
-              type="text"
-              placeholder="Prénom *"
-              className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
-            />
-            <input
-              type="tel"
-              placeholder="Téléphone *"
-              className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
-            />
-            <input
-              type="text"
-              placeholder="Domaine de compétence *"
-              className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 col-span-full"
-            />
-          </motion.div>
+      className="w-full bg-[url('/benevole/bg-benevole.png')] bg-cover bg-center py-16 px-16"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 1.2 }}
+    >
+      <h1 className="text-white text-6xl md:text-4xl font-bold mx-auto pl-8">
+        Formulaire
+        <div className="w-44 md:w-44 h-1 bg-yellow-300 mt-2"></div>
+      </h1>
+      <div className="mx-auto p-8">
+        {/* Form Fields */}
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-2 text-gray-800 gap-6 mb-8"
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
+        >
+          <input
+            type="text"
+            placeholder="Nom *"
+            className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          />
+          <input
+            type="email"
+            placeholder="Email *"
+            className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          />
+          <input
+            type="text"
+            placeholder="Prénom *"
+            className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          />
+          <input
+            type="tel"
+            placeholder="Téléphone *"
+            className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          />
+          <input
+            type="text"
+            placeholder="Domaine de compétence *"
+            className="p-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 col-span-full"
+          />
+        </motion.div>
 
-          {/* Categories */}
-          <motion.div
-            className="grid grid-cols-1 md:grid-cols-3 text-white gap-8"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.7 }}
-          >
-            {/* Foyer */}
-            <div>
-              <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
-                FOYER
-              </h2>
-              <div className="space-y-2">
-                {[
-                  "Soutien Scolaire",
-                  "Accompagnement des enfants",
-                  "Organisations d’évènements",
-                  "Coaching professionnel",
-                  "Animation des ateliers",
-                  "Autres (texte libre)",
-                ].map((item, index) => (
-                  <motion.button
-                    key={index}
-                    className={`w-full text-left p-4 rounded-full border ${
-                      item === "Accompagnement des enfants"
-                        ? "bg-yellow-300 border-black text-2xl font-medium text-center text-red-600"
-                        : "border-yellow-300 text-2xl font-medium text-center bg-red-600 text-white-300"
-                    }`}
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                  >
-                    {item}
-                  </motion.button>
-                ))}
-              </div>
+        {/* Categories */}
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-3 text-white gap-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1.7 }}
+        >
+          {/* Foyer */}
+          <div>
+            <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
+              FOYER
+            </h2>
+            <div className="space-y-2 grid sm:text-base sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1 gap-2">
+              {[
+                "Soutien Scolaire",
+                "Accompagnement des enfants",
+                "Organisations d’évènements",
+                "Coaching professionnel",
+                "Animation des ateliers",
+                "Autres (texte libre)",
+              ].map((item, index) => (
+                <motion.button
+                  key={index}
+                  className={`w-full text-left p-4 rounded-full border ${
+                    selectedButton === item
+                      ? "bg-yellow-300 border-black text-2xl font-medium text-center text-red-600"
+                      : "border-yellow-300 text-2xl font-medium text-center bg-red-600 text-white-300"
+                  }`}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  onClick={() => handleButtonClick(item)}
+                >
+                  {item}
+                </motion.button>
+              ))}
             </div>
+          </div>
 
-            {/* École */}
-            <div>
-              <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
-                ÉCOLE
-              </h2>
-              <div className="space-y-2">
-                {[
-                  "Soutien Scolaire",
-                  "Soutien administratif",
-                  "Animation Ateliers",
-                  "Suivi psychologique",
-                  "Coaching professionnel",
-                  "Formation pédagogique",
-                  "Autres (texte libre)",
-                ].map((item, index) => (
-                  <motion.button
-                    key={index}
-                    className="w-full text-center text-2xl font-medium p-4 rounded-full bg-red-600 border border-yellow-300 text-white-300"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                  >
-                    {item}
-                  </motion.button>
-                ))}
-              </div>
+          {/* École */}
+          <div>
+            <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
+              ÉCOLE
+            </h2>
+            <div className="space-y-2 grid sm:text-base sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1 gap-2">
+              {[
+                "Soutien Scolaire",
+                "Soutien administratif",
+                "Animation Ateliers",
+                "Suivi psychologique",
+                "Coaching professionnel",
+                "Formation pédagogique",
+                "Autres (texte libre)",
+              ].map((item, index) => (
+                <motion.button
+                  key={index}
+                  className={`w-full text-center text-2xl font-medium p-4 rounded-full border ${
+                    selectedButton === item
+                      ? "bg-yellow-300 border-black text-red-600"
+                      : "bg-red-600 border-yellow-300 text-white-300"
+                  }`}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  onClick={() => handleButtonClick(item)}
+                >
+                  {item}
+                </motion.button>
+              ))}
             </div>
+          </div>
 
-            {/* Centre de formation et d'insertion */}
-            <div>
-              <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
-                CENTRE DE FORMATION ET D’INSERTION
-              </h2>
-              <div className="space-y-2">
-                {[
-                  "Coaching pédagogique",
-                  "Cours de communication et soft-skills",
-                  "Formation en métiers d’hôtellerie",
-                  "Autres (texte libre)",
-                ].map((item, index) => (
-                  <motion.button
-                    key={index}
-                    className="w-full text-2xl font-medium p-4 rounded-full text-center bg-red-600 border border-yellow-300 text-white-300"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                  >
-                    {item}
-                  </motion.button>
-                ))}
-              </div>
+          {/* Centre de formation et d'insertion */}
+          <div>
+            <h2 className="text-xl font-bold text-center bg-black border border-white text-white py-4 mb-4 rounded-full">
+              CENTRE DE FORMATION ET D’INSERTION
+            </h2>
+            <div className="space-y-2 grid sm:text-base sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1 gap-2">
+              {[
+                "Coaching pédagogique",
+                "Cours de communication et soft-skills",
+                "Formation en métiers d’hôtellerie",
+                "Autres (texte libre)",
+              ].map((item, index) => (
+                <motion.button
+                  key={index}
+                  className={`w-full text-2xl font-medium p-4 rounded-full text-center border ${
+                    selectedButton === item
+                      ? "bg-yellow-300 border-black text-red-600"
+                      : "bg-red-600 border-yellow-300 text-white-300"
+                  }`}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  onClick={() => handleButtonClick(item)}
+                >
+                  {item}
+                </motion.button>
+              ))}
             </div>
-          </motion.div>
-          <motion.button
-            className="bg-black text-2xl font-semibold text-white border border-white-300 py-4 px-8 mt-8 rounded-full shadow-md hover:bg-gray-700 transition"
-            whileHover={{ scale: 1.1, backgroundColor: "#333333" }}
-            whileTap={{ scale: 0.95 }}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 2 }}
-          >
-            envoyer
-          </motion.button>
-        </div>
-      </motion.div>
+          </div>
+        </motion.div>
+        <motion.button
+          className="bg-black text-2xl font-semibold text-white border border-white-300 py-4 px-8 mt-8 rounded-full shadow-md hover:bg-gray-700 transition"
+          whileHover={{ scale: 1.1, backgroundColor: "#333333" }}
+          whileTap={{ scale: 0.95 }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, delay: 2 }}
+        >
+          envoyer
+        </motion.button>
+      </div>
+    </motion.div>
     </main>
   );
 }
