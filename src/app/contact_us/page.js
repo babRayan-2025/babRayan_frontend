cb4f2e8f535fb873cfec10a9fbd02c3cf815d98c
yassine <yassineova@gmail.com>
2025-04-03 15:59:35 +0000

    add contact us API

--- src/app/contact_us/page.js
+++ src/app/contact_us/page.js
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const fadeIn = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
@@ -16,9 +19,36 @@ export default function Contact() {
     message: "",
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form submitted:", formData);
+    try {
+      const response = await fetch('https://api-mmcansh33q-uc.a.run.app/v1/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nom: formData.nom,
+          telephone: formData.telephone,
+          email: formData.email,
+          message: formData.message
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      toast.success("Message envoyé avec succès");
+      setFormData({
+        nom: "",
+        telephone: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   const handleChange = (e) => {
@@ -30,12 +60,18 @@ export default function Contact() {
 
   return (
     <main className="bg-[url('/devenirPartenaire/background-partenaire.png')] bg-cover bg-center">
-      <motion.h1
-        className="p-4 text-2xl md:text-4xl font-bold text-center my-8 relative"
-        variants={fadeIn} // Apply fadeIn animation
-        initial="initial"
-        animate="animate"
-      >
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <motion.h1 className="p-4 text-2xl md:text-4xl font-bold text-center my-8 relative" variants={fadeIn} initial="initial" animate="animate">
         NOUS CONTACTER
         <div className="w-24 md:w-48 h-1 bg-yellow-200 absolute left-1/2 transform -translate-x-1/2 mt-2"></div>
       </motion.h1>
@@ -44,13 +80,7 @@ export default function Contact() {
         <div className=" bg-yellow-300 border border-gray-600 rounded-3xl text-center md:text-center place-content-center mb-6 md:mb-0 p-8">
           <h2 className="text-5xl font-bold text-red-700 mb-4 flex items-center justify-center">
             <span className="mr-2">
-              <Image
-                src="/contact/localisation.svg"
-                alt="call"
-                width={70}
-                height={70}
-                className="object-cover"
-              />
+              <Image src="/contact/localisation.svg" alt="call" width={70} height={70} className="object-cover" />
             </span>{" "}
             Adresse
           </h2>
@@ -60,13 +90,7 @@ export default function Contact() {
 
           <h2 className="text-5xl font-bold text-red-700 mt-6 mb-4 flex items-center justify-center ">
             <span className="mr-2">
-              <Image
-                src="/contact/appel.svg"
-                alt="call"
-                width={60}
-                height={60}
-                className="object-cover"
-              />
+              <Image src="/contact/appel.svg" alt="call" width={60} height={60} className="object-cover" />
             </span>{" "}
             Téléphone
           </h2>
@@ -77,13 +101,7 @@ export default function Contact() {
           </p>
 
           <div className="text-5xl font-bold text-red-700 mt-6 mb-4 flex items-center justify-center">
-            <Image
-              src="/contact/mail.svg"
-              alt="call"
-              width={60}
-              height={60}
-              className="object-cover"
-            />
+            <Image src="/contact/mail.svg" alt="call" width={60} height={60} className="object-cover" />
             <span className="mr-2"></span> Mail
           </div>
           <p className="text-gray-800 text-xl font-medium">
@@ -97,36 +115,17 @@ export default function Contact() {
         <div className="items-center  bg-yellow-300 border border-gray-600 rounded-3xl place-content-center pt-6 md:p-8">
           <h2 className="text-5xl font-bold text-red-700 mb-8 text-center flex items-center justify-center ">
             <span className="mr-2 ">
-              <Image
-                src="/contact/localisation.svg"
-                alt="call"
-                width={70}
-                height={70}
-                className="object-cover"
-              />
+              <Image src="/contact/localisation.svg" alt="call" width={70} height={70} className="object-cover" />
             </span>{" "}
             Localisation
           </h2>
           <div className="w-full max-w-sm md:max-w-md bg-white border border-gray-600 rounded-lg shadow-md overflow-hidden">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.846981346241!2d-7.632492684855834!3d33.592882980730195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDM1JzM0LjQiTiA3wrAzNyc1Ny4wIlc!5e0!3m2!1sen!2sma!4v1635789876543!5m2!1sen!2sma"
-              width={600}
-              height={400}
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              className="rounded-lg"
-            ></iframe>
-            {/* <Image
-            src="/map.png" 
-            alt="Association Bab Rayan Location"
-            width={600}
-            height={400}
-            className="w-full h-auto object-cover"
-          /> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.846981346241!2d-7.632492684855834!3d33.592882980730195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDM1JzM0LjQiTiA3wrAzNyc1Ny4wIlc!5e0!3m2!1sen!2sma!4v1635789876543!5m2!1sen!2sma"
+              width={600} height={400} style={{ border: 0 }} allowFullScreen="" loading="lazy" className="rounded-lg" ></iframe>
           </div>
         </div>
       </div>
+
       {/* Form section */}
       <motion.div
         initial="hidden"
@@ -187,16 +186,14 @@ export default function Contact() {
                 value={formData.message}
                 onChange={handleChange}
                 rows={6}
+                required
                 className="w-full px-4 py-3 rounded-3xl text-gray-700 text-lg"
               />
             </div>
 
             <div>
-              <button
-                type="submit"
-                className="px-8 py-2 border border-cyan-50 bg-yellow-300 hover:bg-yellow-400 text-red-600 rounded-full text-xl font-bold transition-colors duration-200"
-              >
-                envoyer
+              <button type="submit" className="px-8 py-2 border border-cyan-50 bg-yellow-300 hover:bg-yellow-400 text-red-600 rounded-full text-xl font-bold transition-colors duration-200">
+                Envoyer
               </button>
             </div>
           </form>
